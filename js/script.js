3e8145041338d4418c4d78587852720ead99fe63
deborachoi3 <bbybokchoi@gmail.com>
2019-10-29 01:12:32 +0000

    rockpaperscissors

--- js/script.js
+++ js/script.js
@@ -4,20 +4,35 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-$(".button").click(function(){
-    let input = $("#shoot").val();
+console.log("hi")
+$("#shoot").click(function(){
+    let input = $("#input").val();
+     let r = Math.random()
+        if(r < 0.33){
+            let computerChoice = "rock"
+        }
+        else if (r < 0.67){
+            let computerChoice = "paper"
+        }
+        else{
+            let computerChoice = "scissors"
+        }
 
+$("#userChoice").text(input)
+$("#computerChoice").text("hi")
+ /*console.log(userInput);
+    if (userInput == "rock") && (r < 0.99) || (userInput == "paper") && (r < 0.33) || (userInput == "scissors") && (r < 0.66) {
+        $("#text").html("User wins!");
+    }
+    else if (userInput == "rock" && r < 0.67) || (userInput == "paper") && (r < 0.99) || (userInput == "scisssors") && (r < 0.33){
+        $("#text").html("Computer wins");
+    }
+    else if (userInput = "rock" && r < 0.33){
+       $("#text").html("It's a tie");
+    }*/
 });
-    let r = Math.random()
-        if(r <0.33){
-            let computer choice = "rock"
-        }
-        else if (r<0.67){
-            let computer choice = "paper"
-        }
-        else
-            let computer choice = "scissors"
-        }
+
+
 
 
 
